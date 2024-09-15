510cfeaa5867a82a1b1691f330ffb9d182759963
onsnirwana <onsnirwana@gmail.com>
2024-09-16 00:47:39 +0700

    first commit

---
+++ nirwana.js
@@ -0,0 +1,6 @@
+function showAlert() {
+            alert("Halo! Ini adalah alert dari JavaScript.");
+        }
+
+        // Memanggil fungsi showAlert saat halaman dimuat
+        window.onload = showAlert;
